4b308f4b7ac94515a962392101ed4549c1d6a352
chrisleuzzi <118318039+chrisleuzzi@users.noreply.github.com>
2022-11-29 18:59:01 -0500

    Update main.rs
    Fixed src

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,13 @@ struct Color {
     blue: u8,
 }
 
+impl Display for Color {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "red: {}, green: {}, blue: {}",
+               self.red, self.green, self.blue)
+}
+}
+
 fn main() {
     for city in [
         City { name: "Glassboro", lat: 39.702892, lon: -75.111839 },
@@ -44,6 +51,6 @@ fn main() {
         Color { red: 0, green: 0, blue: 0 },
     ].iter() {
         // Hint : Fix the code so you can print it using {}
-        println!("{:?}", *color);
+        println!("{}", *color);
     }
 }
